a2e613b9e7b33aaf35f58f68e65ef2f4ace5db35
JBravo3122 <165884087+JBravo3122@users.noreply.github.com>
2026-01-19 12:38:33 +0800

    Cleaned up Create Announcement imports

--- Scripts/Create Announcement/Script1768541490206.groovy
+++ Scripts/Create Announcement/Script1768541490206.groovy
@@ -1,28 +1,13 @@
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
-import org.openqa.selenium.By as By
-import org.openqa.selenium.WebDriver as WebDriver
-import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
-import org.openqa.selenium.support.ui.WebDriverWait as WebDriverWait
-import org.openqa.selenium.support.ui.ExpectedConditions as ExpectedConditions
-import java.time.Duration as Duration
-import java.nio.file.Files as Files
-import java.nio.file.Paths as Paths
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.util.KeywordUtil
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import com.kms.katalon.core.webui.driver.DriverFactory
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.openqa.selenium.support.ui.ExpectedConditions
+import java.time.Duration
+import java.nio.file.Files
+import java.nio.file.Paths
 
 Map<String, String> env = [:]
 
